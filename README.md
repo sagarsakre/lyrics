9895094a0253417f61b40fe96bd2ace2977ed354
Sagar Sakre <sagarsakre87@gmail.com>
2014-12-05 19:07:19 +0530

    Lyrics from bash

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ lyrics
 ======
 
 Written a small python script which will fetch the lyrics for the given string from web and show it on the console
+
+Just put it in any folder and export the PATH variable. Whenever you feel like checking the song lyrics
+you can just type 
+$~ lyrics <song-name>
+Thats it. Enjoy songs !!!
---
+++ lyrics.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import HTMLParser
+import urllib
+import json
+import sys
+import re
+
+base = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&'
+query = urllib.urlencode({'q' :  sys.argv[1]+"lyrics"})
+response = urllib.urlopen(base + query).read()
+data = json.loads(response)
+print
+print 'Trying to get data from'
+print data['responseData']['results'][0]['url']
+print
+print 'Enjoy the Lyrics of',sys.argv[1],'brought to you by Sagar Sakre'
+urlText = []
+
+#Define HTML Parser
+class parseText(HTMLParser.HTMLParser):
+        
+    def handle_data(self, data):
+        if data != '\n':
+            urlText.append(data)
+    
+
+#Create instance of HTML parser
+lParser = parseText()
+
+#thisurl = data['responseData']['results'][0]['url']
+#Feed HTML file into parser
+lParser.feed(urllib.urlopen(data['responseData']['results'][0]['url']).read())
+lParser.close()
+#f1=open('./testfile', 'w+')
+#for item in urlText:
+#    f1.write(item)
+#    print item
+first_twelve = urlText[51:600]
+for each_line in first_twelve:
+    if "/Android|webOS|iPhone|iPod|iPad|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent" in each_line:
+        break
+    else:
+        print each_line
+#if prog.match(each_line):
